4ebdb8eb717e12faf1c38a5d691ad041b28d59fd
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2024-03-05 16:47:40 +0100

    cycle trough all cities and add them to a file

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 from fp.fp import FreeProxy
-import json
-
-url = "https://www.ubereats.com/be/category/gent-flandre-orientale/fast-food"
-
-#proxy = FreeProxy(rand=True, timeout=1).get()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
+#proxy = FreeProxy(rand=True, timeout=1).get()
 
-try:
-    response = requests.get(url, headers=headers, timeout=10)
-    response.raise_for_status() 
-except requests.exceptions.RequestException as e:
-    print("An error occurred:", e)
-    exit(1)
+cities = ["antwerpen-anvers", "brussel-bruxelles-capitale", "gent-flandre-orientale", "li%C3%A8ge-li%C3%A8ge", "louvain-la-neuve-brabant-wallon", "namur-namur"]
 
-soup = BeautifulSoup(response.content, "html.parser")
+for city in cities:
+    if city == "li%C3%A8ge-li%C3%A8ge":
+        print("\n\nLIÈGE\n")
+        with open("restaurants.txt", "a", encoding="utf-8") as file:
+            file.write("\n\nLIÈGE\n")
+            file.close()
+    else:
+        print(f"\n\n{city.upper()}\n")
+        with open("restaurants.txt", "a", encoding="utf-8") as file:
+            file.write(f"\n\n{city.upper()}\n")
+            file.close()
+    url = f"https://www.ubereats.com/be/city/{city}"
 
-shops = soup.find_all('div', class_=["ak", "bl"])
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status() 
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+        exit(1)
 
-for shop in shops:
-    names = shop.find_all('h3', class_=["ko", "ct", "ae", "ag"])
-    links = shop.find_all('a')
-    for name, link in zip(names, links):
-        restaurant_name = name.text
-        page_link = "https://www.ubereats.com" + link['href']
-        print(restaurant_name, page_link)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    shops = soup.find_all('a', {"data-test": "store-link"})
+
+    for shop in shops:
+        path = shop['href']
+        page_link = "https://www.ubereats.com" + path
+        names = shop.find_all('h3')
+        for name in names:
+            restaurant_name = name.text
+            with open("restaurants.txt", "a", encoding="utf-8") as file:
+                file.write(f"{restaurant_name}, {page_link}\n")
+                file.close()
